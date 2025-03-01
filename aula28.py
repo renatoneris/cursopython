8e3b059a88c44a23b507ce0eef9a0908249263a1
Renato <renato_fuc@hotmail.com>
2025-02-28 22:44:39 -0400

    aula28.py

--- aula28.py
+++ aula28.py
@@ -22,16 +22,17 @@ if nome and idade:
 
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[::-1]}')
-
+    
     if ' ' in nome:
         print('Seu nome contém espaços')
     else:
         print('Seu nome NÃO contém espaços')
-
+        
     print(f'Seu nome contém "{len(nome)}" letras')
 
     print(f'A primeira letra do seu nome é "{nome[0]}"')
     print(f'A útima letra do seu nome é "{nome[-1]}"')
-
+    
 else:
     print('Desculpe, você deixou campos varios')
+    
